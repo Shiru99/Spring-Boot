66bdb2a2aff495af34210a8bedfa10a84f06df2a
Shiru99 <shriram.ghadge1999@gmail.com>
2021-07-08 16:26:28 +0530

    initiated using spring initializr

---
+++ spring-boot-intro/src/main/java/io/summer/springbootintro/SpringBootIntroApplication.java
@@ -0,0 +1,12 @@
+package io.summer.springbootintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIntroApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIntroApplication.class, args);
+	}
+
+}
